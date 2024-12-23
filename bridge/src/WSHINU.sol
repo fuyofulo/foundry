6c58df9edcb1eeeb225f22b67f494ec0e6dfb49e
fuyofulo <pheonixdiaz625@gmail.com>
2024-12-23 19:21:18 +0530

    lol

--- bridge/src/WSHINU.sol
+++ bridge/src/WSHINU.sol
@@ -2,18 +2,19 @@
 pragma solidity ^0.8.13;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WSHINU is ERC20 { 
+contract WSHINU is ERC20, Ownable { 
 
-    constructor () ERC20("WSHINU", "WSHINU") {
+    constructor () ERC20("WSHINU", "WSHINU") Ownable(msg.sender) {
 
     }
 
-    function mint(address to, uint256 amount) public {
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function burn(address from, uint256 amount) public {
+    function burn(address from, uint256 amount) public onlyOwner {
         _burn(from, amount);
     }
 
